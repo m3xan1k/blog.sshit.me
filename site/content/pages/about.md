e816b85d688fecc1386b3e30017a5ae4e4321583
Sergey Shevtsov <dev.serzh@gmail.com>
2022-02-06 22:28:19 +0300

    add content

---
+++ site/content/pages/about.md
@@ -0,0 +1,6 @@
+Title: About
+Slug: about
+
+>
+> Snippets from IT-engineer with more than 10 years expirience
+>
---
+++ site/pelicanconf.py
@@ -0,0 +1,28 @@
+AUTHOR = 'm3xan1k'
+SITENAME = 'tech blog'
+SITEURL = 'https://blog.sshit.ovh'
+
+PATH = 'content'
+ARTICLE_PATHS = ['blog']
+ARTICLE_SAVE_AS = '{slug}.html'
+ARTICLE_URL = '{slug}.html'
+
+TIMEZONE = 'Europe/Moscow'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+# DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
